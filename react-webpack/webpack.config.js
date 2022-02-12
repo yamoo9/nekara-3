80472f2138cf5f9ae919da49c35e1c7c00e4b285
yamoo9 <yamoog@naver.com>
2022-02-12 09:23:24 +0900

    CSS 모듈 설정 제외

--- react-webpack/webpack.config.js
+++ react-webpack/webpack.config.js
@@ -79,20 +79,7 @@ const webpackConfig = ({ development, production, analyzer }) => {
           test: /\.css$/i,
           use: [
             development ? 'style-loader' : CssExtractPlugin.loader,
-            {
-              loader: 'css-loader',
-              options: {
-                mode: "local",
-                auto: true,
-                exportGlobals: true,
-                localIdentName: "[path][name]__[local]--[hash:base64:5]",
-                localIdentContext: path.resolve(__dirname, "src"),
-                localIdentHashSalt: "my-custom-hash",
-                namedExport: true,
-                exportLocalsConvention: "camelCase",
-                exportOnlyLocals: false,
-              }
-            },
+            'css-loader'
           ],
         },
       ].filter(Boolean),
