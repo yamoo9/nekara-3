6344c5a43dbe667f37897c322304f086ade4256c
yamoo9 <yamoog@naver.com>
2022-02-18 18:44:47 +0900

    useCallback vs useMemo
    - 이벤트 핸들러 prop 기억하기
    - 컴포넌트를 포함한 어떤 값 기억하기

--- react-vite/src/components/Counter/container.jsx
+++ react-vite/src/components/Counter/container.jsx
@@ -1,31 +1,62 @@
 import './container.css';
-import { useState } from 'react';
+import { useState, useCallback, useMemo } from 'react';
 
 import { CountHeadline } from './headline';
 import { CountOutput } from './output';
 import { Button } from './button';
 
-// useCallback vs useMemo
+// useCallback (함수 값 기억하기) vs useMemo (어떤 값(컴포넌트도 가능)이든 기억 가능)
 
 export function CountContainer() {
-  
   const [headline, setHeadline] = useState('Counter');
-  const handleChangeHeadline = () => setHeadline('Updated Counter Headnline');
+  const handleChangeHeadline = useCallback(
+    () => setHeadline('Updated Counter Headnline'),
+    []
+  );
 
   const [count, setCount] = useState(10);
-  const handleDecrementCount = () => setCount((prevCount) => prevCount - 1);
-  const handleIncrementCount = () => setCount((prevCount) => prevCount + 1);
+  const handleDecrementCount = useCallback(
+    () => setCount((prevCount) => prevCount - 1),
+    []
+  );
+  const handleIncrementCount = useCallback(
+    () => setCount((prevCount) => prevCount + 1),
+    []
+  );
+
+
+  // 하위 중첩된 컴포넌트(값) 기억하기
+  const MemoizedCountHeadline = useMemo(() => (
+    <CountHeadline>{headline}</CountHeadline>
+  ), [headline]);
+  
+  const MemoizedChangeHeadlineButton = useMemo(() => (
+    <Button onClick={handleChangeHeadline}>change headline</Button>
+  ), [handleChangeHeadline]);
+
+  const MemoizedCountOutput = useMemo(() => (
+    <CountOutput> {count} </CountOutput>
+  ), [count]);
+  
+  const MemoizedDecrementButton = useMemo(() => (
+    <Button onClick={handleDecrementCount}> - </Button>
+  ), [handleDecrementCount]);
+  
+  const MemoizedIncrementButton = useMemo(() => (
+    <Button onClick={handleIncrementCount}> + </Button>
+  ), [handleIncrementCount]);
+
 
   return (
     <div className="counter">
       <div className="counter-headline">
-        <CountHeadline> {headline} </CountHeadline>
-        <Button onClick={handleChangeHeadline}>change headline</Button>
+        {MemoizedCountHeadline}
+        {MemoizedChangeHeadlineButton}
       </div>
       <div>
-        <Button onClick={handleDecrementCount}> - </Button>
-        <CountOutput> {count} </CountOutput>
-        <Button onClick={handleIncrementCount}> + </Button>
+        {MemoizedDecrementButton}
+        {MemoizedCountOutput}
+        {MemoizedIncrementButton}
       </div>
     </div>
   );
