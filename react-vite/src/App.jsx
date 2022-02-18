5c6a9524b756ed8abad246a28d67a04b3553fdb9
yamoo9 <yamoog@naver.com>
2022-02-18 15:40:18 +0900

    useRef의 활용 : 콜백 ref

--- react-vite/src/App.jsx
+++ react-vite/src/App.jsx
@@ -8,25 +8,14 @@ import { List } from './statful/List';
 
 export default function App(props) {
   
-  // 1. tuple data type [state, setState]
-  // 2. intial value (lazy intial value)
-  const [appName, setAppName] = React.useState(props.appName);
+  const [appName, /* setAppName */] = React.useState(props.appName);
 
-  // 1. side effect function (subscription)
-  // 2. dependency array (condition)
-  // 3. cleanup (unsubscription)
-  React.useEffect(() => {
-    console.log(`changed app name: ${appName}`);
+  // React.useEffect(() => {
+  //   console.log(`changed app name: ${appName}`);
+  //   const clearId = setInterval(() => setAppName((prevState) => prevState + `😃`), 3000);
+  //   return () => clearInterval(clearId);
 
-    // 사이드 이펙트 설정
-    // 타임 컨트롤 (React에 의해 관리되는 것이 아님 : 부수 효과)
-    const clearId = setInterval(() => setAppName((prevState) => prevState + `😃`), 3000);
-
-    return /* cleanup */ () => {
-      clearInterval(clearId);
-    }
-
-  }, [appName]);
+  // }, [appName]);
 
   return (
     <div className="App">
