6a5c625e981e00d6407415621b54333cbf8e835f
yamoo9 <yamoog@naver.com>
2022-02-25 14:41:17 +0900

    인증 컨텍스트 상태 유지 (with JS Cookie)

--- react-router-v6/src/contexts/auth.js
+++ react-router-v6/src/contexts/auth.js
@@ -4,8 +4,11 @@
 // Authentication (인증) 상태 관리
 // 컨텍스트 내에 포함된 컴포넌트 사이 상태 공유/관리
 
-import { createContext, useReducer, useMemo, useContext } from 'react';
+import { createContext, useReducer, useMemo, useContext, useEffect } from 'react';
+import Cookies from 'js-cookie';
 
+// 쿠키의 키(key) 값 (상수)
+const AUTH_COOKIE = 'AUTH_COOKIE';
 
 // 컨텍스트 생성
 const AuthContext = createContext();
@@ -16,6 +19,11 @@ const AuthContext = createContext();
 // - permission → 권한 (읽기/쓰기/수정/삭제 등)
 // - signIn → 입력받은 계정 정보 : (로그인) 인증 처리 메서드
 // - signOut → 인증된 사용자를 해제 : (로그아웃) 인증 해제 메서드
+
+// 상태 유지
+// 로그인 후, 파기(날짜, 사용자 상호작용 등) 되기 전까지는 상태가 유지되어야 한다.
+// - localStorage
+// - Cookies (js-cookie)
 
 // 초기 상태 값
 const initialState = {
@@ -45,11 +53,43 @@ const reducer = (state, action) => {
   return state;
 }
 
+// 지연된 초기화 → 초기 상태 설정
+const lazyInit = () => {
+  // 저장된 쿠키 값을 가져온다.
+  const authCookie = Cookies.get(AUTH_COOKIE);
+
+  // 반환할 초기 상태 값
+  let returnValue = {};
+
+  // 저장된 쿠키 값이 있다면?
+  if (authCookie) {
+    returnValue = JSON.parse(authCookie);
+  }
+
+  // 처리 후에 값을 반환
+  return { 
+    ...initialState,
+    ...returnValue,
+  };
+}
+
 
 // 생성된 컨텍스트 값 공급자(provider)
 export const AuthProvider = (props) => {
 
-  const [{ currentUser, permission }, dispatch] = useReducer(reducer, initialState);
+  const [{ currentUser, permission }, dispatch] = useReducer(reducer, initialState, lazyInit);
+
+  useEffect(() => {
+
+    if (currentUser) {
+      Cookies.set(AUTH_COOKIE, JSON.stringify({currentUser, permission}), { expires: 1 });
+    } else {
+      Cookies.remove(AUTH_COOKIE);
+    }
+
+  }, [currentUser, permission]);
+
+  // ------------------------------------------------------------------------------------------
 
   const signIn = (currentUser, permission = 'member') => 
     dispatch({
