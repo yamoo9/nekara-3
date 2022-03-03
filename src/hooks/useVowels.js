8957daee6e789f5ab2ba696075d31999d04ef457
yamoo9 <yamoog@naver.com>
2022-03-03 18:41:26 +0900

    Netlify 서버시 배포 시 발생한 문제 해결
    - useVowels 훅에서 호출하는 process.env 환경 변수를 찾을 수 없음
    - .env 파일에 작성되어 있는 API url을 useVowels 훅에서 사용하도록 설정

--- src/hooks/useVowels.js
+++ src/hooks/useVowels.js
@@ -1,24 +1,26 @@
 import useSWR from 'swr';
 
-const {REACT_APP_API: ENDPOINT} = process.env;
-const fetcher = (...args) => fetch(...args).then(response => response.json());
+const { REACT_APP_API: ENDPOINT } = process.env;
+const fetcher = (...args) => fetch(...args).then((response) => response.json());
+
+const endpoint = ENDPOINT ?? 'https://my-json-server.typicode.com/yamoo9/assets/vowels';
 
 export const useVowels = () => {
-  const { error, data, mutate } = useSWR(ENDPOINT, fetcher);
+  const { error, data, mutate } = useSWR(endpoint, fetcher);
   return {
     error,
     vowels: data,
     setVowels: mutate,
     loading: !data && !error,
-  }
-}
+  };
+};
 
 export const useVowel = (id) => {
-  const { error, data, mutate } = useSWR(`${ENDPOINT}/${id}`, fetcher);
+  const { error, data, mutate } = useSWR(`${endpoint}/${id}`, fetcher);
   return {
     error,
     vowel: data,
     setVowel: mutate,
     loading: !data && !error,
-  }
-}
+  };
+};
